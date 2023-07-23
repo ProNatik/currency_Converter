284573bbd8be199fd98ed68376f188f0e36a858e
Nathan <nathan.durand77@outlook.fr>
2023-07-23 11:11:37 +0200

    finish again ?

--- api/resources/views/doc.blade.php
+++ api/resources/views/doc.blade.php
@@ -16,14 +16,14 @@
         <h1>Doc : Comment utiliser l'API Currency Converter</h1>
         <h2>Devises</h2>
         <div style="margin-left: 15px">
-            <p>Afficher les Devises : <strong>(GET) http://localhost:8000/api/devises</strong></p>
+            <p>Afficher les Devises : <strong>(GET) http://localhost:8000/api/devises</strong> avec pour paramètre : {value}</p>
             <p>Créer un Devise : <strong>(POST) http://localhost:8000/api/devises</strong></p>
         </div>
         <h2>Paires</h2>
         <div style="margin-left: 15px">
             <p>Afficher les Paires : <strong>(GET) http://localhost:8000/api/paires</strong></p>
             <p>Afficher une Paires : <strong>(GET) http://localhost:8000/api/paires/{from_devise}/{to_devise}</strong></p>
-            <p>Créer une Paires : <strong>(POST) http://localhost:8000/api/paires</strong></p>
+            <p>Créer une Paires : <strong>(POST) http://localhost:8000/api/paires</strong> avec pour paramètres : {from_devise, to_devise, value}</p>
             <p>Modifier une Paires : <strong>(PUT) http://localhost:8000/api/paires/{from_devise}/{to_devise}</strong> avec pour paramètre : {value}</p>
             <p>Delete une Paires : <strong>(DELETE) http://localhost:8000/api/paires/{from_devise}/{to_devise}</strong></p>
             <p>Afficher la valeur modifier par la Paire : <strong>(GET) http://localhost:8000/api/paires/{from_devise}/{to_devise}/{value}</strong></p>
